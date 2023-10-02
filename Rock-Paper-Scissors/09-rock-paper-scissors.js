8e2734d91f87c00a42e368291bb26da8598f9b81
AbdullahAshraff <125521810+AbdullahAshraff@users.noreply.github.com>
2023-10-02 23:39:36 +0300

    1.0 animation edited - clean js code

--- Rock-Paper-Scissors/09-rock-paper-scissors.js
+++ Rock-Paper-Scissors/09-rock-paper-scissors.js
@@ -1,104 +1,111 @@
-let ls = ['rock','paper','scissors']
-let ob = JSON.parse(localStorage.getItem('score')) ?? {wins : 0, ties : 0, loses:0}
+let ls = ['rock', 'paper', 'scissors'];
+let ob = JSON.parse(localStorage.getItem('score')) ?? {
+  wins: 0,
+  ties: 0,
+  loses: 0,
+};
+let scoreStr = function () {
+  return `Wins:${ob.wins} Ties:${ob.ties} Loses:${ob.loses}`
+};
 let imgdict = {
-  rock:'<img src="..\\footage\\rock-emoji.png" alt="rock">',
-  paper:'<img src="..\\footage\\paper-emoji.png" alt="paper">',
-  scissors:'<img src="..\\footage\\scissors-emoji.png" alt="scissors">',
-  question:'<img src="..\\footage\\question-emoji.png" alt="unknown">',
-  animated:'<img src="..\\footage\\thirdTry.gif" alt="waiting" style="">',
-}
+  rock: '<img src="..\\footage\\rock-emoji.png" alt="rock">',
+  paper: '<img src="..\\footage\\paper-emoji.png" alt="paper">',
+  scissors: '<img src="..\\footage\\scissors-emoji.png" alt="scissors">',
+  question: '<img src="..\\footage\\question-emoji.png" alt="unknown">',
+  animated: '<img src="..\\footage\\fourthTry.gif" alt="waiting" style="">',
+};
 
-const allButtons = document.querySelectorAll('.user-move-column button')
-allButtons.forEach(element => {
-  element.classList.remove('move-selected')
-  element.classList.add('move-notselected')
-});
-document.querySelector('.com-move-frame').classList.remove('move-selected')
-document.querySelector('.com-move-frame').classList.add('move-notselected')
+const allUserButtons = {
+  ls: document.querySelectorAll('.user-move-column button'),
+  deselectAll: function () {
+    this.ls.forEach(el => {
+      el.classList.value = 'move-notselected';
+    });
+  },
+  disableAll: function () {
+    allUserButtons.ls.forEach(el => el.disabled = true);
+  },
+  enableAll: function () {
+    allUserButtons.ls.forEach(el => el.disabled = false);
+  },
+};
 
-document.querySelector('.js-score').innerHTML = `Wins:${ob.wins} Ties:${ob.ties} Loses:${ob.loses}`;
+const comFrame = {
+  ls: document.querySelector('.com-move-frame'),
+  deselect: function () {
+    this.ls.classList.value = 'com-move-frame move-notselected';
+  },
+  select: function () {
+    this.ls.classList.value = 'com-move-frame move-selected-com';
+  },
+};
+const scoreDiv = document.querySelector('.js-score');
+const resultDiv = document.querySelector('.js-result');
 
-async function game(plaMove){
-  let ranNum  = Math.floor(Math.random()*3);
+resetWithoutScore()
+
+async function game(plaMove) {
+  let ranNum = Math.floor(Math.random() * 3);
   let comMove = ls[ranNum];
 
-  // Highlighting player move
-  let plaMoveIdx = ls.indexOf(plaMove)
-  let allButton = document.querySelectorAll('.user-move-column button')
-  allButton.forEach(element => {
-    element.classList.remove('move-selected')
-    element.classList.add('move-notselected')
-  });
-
-  allButton [plaMoveIdx].classList.remove('move-notselected')
-  allButton [plaMoveIdx].classList.add('move-selected')
+  // Highlighting player's move
+  let plaMoveIdx = ls.indexOf(plaMove);
+  allUserButtons.deselectAll();
+  allUserButtons.ls[plaMoveIdx].classList.value = 'move-selected';
 
   // waiting and playing animation
+  allUserButtons.disableAll();    // Disable playing again
+  resultDiv.innerHTML = '...';
+  resultDiv.classList.value = 'js-result result result-waiting';
+  comFrame.deselect();
+  comFrame.ls.innerHTML = imgdict['animated']; // waiting gif
 
-  
-  document.querySelector('.com-move-frame').classList.remove('move-selected-com')
-  document.querySelector('.com-move-frame').classList.add('move-notselected')
-
-
-  allButtons.forEach(el =>el.disabled = true);
-  document.querySelector('.js-result').innerHTML = '...';
-  document.querySelector('.js-result')
-    .classList.value = 'js-result result result-waiting';
-  document.querySelector('.com-move-frame').innerHTML = imgdict['animated'];
-  await sleep(((Math.random()*2+1)*1000).toFixed(0));
-
-
+  await sleep(((Math.random() * 2 + 1) * 1000).toFixed(0));
 
   document.querySelector('.players-tip').style.opacity = 0;
-  
-  document.querySelector('.com-move-frame').classList.remove('move-notselected')
-  document.querySelector('.com-move-frame').classList.add('move-selected-com')
+
+  comFrame.select();
 
   // calc Result
-  if (comMove === plaMove){
-    document.querySelector('.js-result').innerHTML = 'Tie!';
-    document.querySelector('.js-result')
-      .classList.value = 'js-result result result-tie';
+  if (comMove === plaMove) {
+    resultDiv.innerHTML = 'Tie!';
+    resultDiv.classList.value = 'js-result result result-tie';
     ob.ties++;
-  }else if (ls[(ranNum+1)%3] === plaMove){
-    document.querySelector('.js-result').innerHTML = 'You Won!';
-    document.querySelector('.js-result')
-      .classList.value = 'js-result result result-won';
+  } else if (ls[(ranNum + 1) % 3] === plaMove) {
+    resultDiv.innerHTML = 'You Won!';
+    resultDiv.classList.value = 'js-result result result-won';
     ob.wins++;
-  }else {
-    document.querySelector('.js-result').innerHTML = 'You Lost!';
-    document.querySelector('.js-result')
-      .classList.value = 'js-result result result-lost';
+  } else {
+    resultDiv.innerHTML = 'You Lost!';
+    resultDiv.classList.value = 'js-result result result-lost';
     ob.loses++;
   }
-  
-  // Show Moves
-  document.querySelector('.com-move-frame').innerHTML = imgdict[comMove];
 
-  
+  comFrame.ls.innerHTML = imgdict[comMove];   // Show Computer Move
+
   // update score
-  document.querySelector('.js-score').innerHTML = `Wins:${ob.wins} Ties:${ob.ties} Loses:${ob.loses}`;
-  localStorage.setItem('score',JSON.stringify(ob));
+  scoreDiv.innerHTML = scoreStr();
+  localStorage.setItem('score', JSON.stringify(ob));
 
-  
-  allButtons.forEach(el =>el.disabled = false);
+  // Enable playing again
+  allUserButtons.enableAll();
 }
 
-function reset(){
+function resetWithoutScore() {
+  comFrame.ls.innerHTML = imgdict['question'];
+  resultDiv.innerHTML = "&nbsp;";
+  scoreDiv.innerHTML = scoreStr();
+  allUserButtons.deselectAll()
+  comFrame.deselect()
+  document.querySelector('.players-tip').style.opacity = 1;
+}
+
+function reset() {
   localStorage.removeItem('score');
-  ob = {wins : 0, ties : 0, loses:0};
-  document.querySelector('.com-move-frame').innerHTML = imgdict['question'];
-  document.querySelector('.js-result').innerHTML = "";
-  document.querySelector('.js-score').innerHTML = `Wins: ${ob.wins} Ties:${ob.ties} Loses: ${ob.loses}`;
-  let allButton = document.querySelectorAll('.user-move-column button')
-  allButton.forEach(element => {
-    element.classList.remove('move-selected')
-    element.classList.add('move-notselected')
-  });
-  
-  document.querySelector('.com-move-frame').classList.remove('move-selected-com')
-  document.querySelector('.com-move-frame').classList.add('move-notselected')
+  ob = { wins: 0, ties: 0, loses: 0 };
+  resetWithoutScore()
 }
+
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
